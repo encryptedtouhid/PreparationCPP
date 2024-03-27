a6376d66046a6c94f256cfcfcfa31ca723c1cc7c
Khaled Md Tuhidul Hossain <me@tuhidulhossain.com>
2024-03-27 14:31:32 +0800

    initial commit

---
+++ PreparationCPP/PreparationCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PreparationCPP
+//
+//  Created by Khaled Md Tuhidul Hossain on 23/03/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    for (int i = 0; i < 10; i++) {
+        std::cout << i << "\n";
+    }
+    return 0;
+}
